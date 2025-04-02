302e331249c0fdd388927d3149bf7ddc7b969811
Egurnova Elena <gamedev409@yandex.ru>
2025-04-02 10:30:07 +0500

    Добавьте файлы проекта.

---
+++ Library/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearOfPublication { get; set; }
+    }
+}
---
+++ Library/ViewModels/MainViewModel.cs
@@ -0,0 +1,96 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Library.ViewModels
+{
+#pragma warning disable
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _author;
+        private int yearOfPublication;
+        private Book _selectedBook;
+
+        public ObservableCollection<Book> Books { get; } = new();   
+        public MainViewModel()
+        {
+            AddCommand = new RelayCommand(AddBook);
+            DeleteCommand = new RelayCommand(DeleteBook);
+        }
+
+        #region properties
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set
+            {
+                _author = value;
+                OnPropertyChanged(nameof(Author));
+            }
+        }
+        public int YearOfPublication
+        {
+            get => yearOfPublication;
+            set
+            {
+                yearOfPublication = value;
+                OnPropertyChanged(nameof(YearOfPublication));
+            }
+        }
+        public Book SelectedBook
+        {
+            get => _selectedBook;
+            set
+            {
+                _selectedBook = value;
+                OnPropertyChanged(nameof(SelectedBook));
+            }
+        }
+        #endregion
+
+        #region ICommand
+        public RelayCommand AddCommand { get; }
+        public RelayCommand DeleteCommand { get; }
+
+        private void AddBook(object parameter)
+        {
+            Books.Add(new Book
+            {
+                Title = Title,
+                Author = Author,
+                YearOfPublication = yearOfPublication,
+            }
+            );
+        }
+        private void DeleteBook(object parameter)
+        {
+            if (SelectedBook != null)
+                Books.Remove(SelectedBook);
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+    }
+}
